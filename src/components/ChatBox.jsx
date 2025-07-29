c0ab91ad8d56b421bd74e9404339ed24ca200d0a
Roberto Marino <197581795+RobertoM23@users.noreply.github.com>
2025-07-29 18:09:08 +0200

    .

--- src/components/ChatBox.jsx
+++
@@ -1,9 +0,0 @@
-export function ChatBox({ messages }) {
-  return (
-    <div className="space-y-1">
-      {messages.map((m, i) => (
-        <div key={i}><b>{m.sender}</b>: {m.message}</div>
-      ))}
-    </div>
-  );
-}
--- src/components/ReviewList.jsx
+++
@@ -1,9 +0,0 @@
-export function ReviewList({ reviews }) {
-  return (
-    <div className="space-y-1">
-      {reviews.map((r, i) => (
-        <div key={i}><b>{r.reviewer}</b>: {r.comment} ({r.rating}/5)</div>
-      ))}
-    </div>
-  );
-}
